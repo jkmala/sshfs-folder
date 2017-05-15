b5669c486c1bfeec1ef9e2dbb6a6d2f2ce64d6af
Juha Kulmala <jkmala@gmail.com>
2017-05-15 15:34:57 +0000

    more

--- modules/sshfs/manifests/init.pp
+++ modules/sshfs/manifests/init.pp
@@ -9,14 +9,15 @@ class sshfs {
 	}
 	
 	mount {"kouludir":
-		name 	=> "/home/xubuntu/koulu/",
-		atboot 	=> true,
-		device 	=> "sshfs#a1402745@myy.haaga-helia.fi:/netapp/nfstiko/u10/a1402745/",
-		ensure 	=> "mounted",
+		require => File['/home/xubuntu/koulu/'],
+		name => "/home/xubuntu/koulu/",
+		atboot => true,
+		device => "sshfs#a1402745@myy.haaga-helia.fi:/netapp/nfstiko/u10/a1402745/",
+		ensure => "mounted",
 		fstype  => "fuse",
 		options => "idmap=user,uid=999,gid=999",
-		dump	=> "0",
-		pass 	=> "0",
+		dump => "0",
+		pass => "0",
 		remounts => false,
 
 	}
